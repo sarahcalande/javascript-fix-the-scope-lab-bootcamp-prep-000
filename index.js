9407232ed2c72fa8765b5b0a9275516ba21d3b79
scalande@yahoo.com <scalande@yahoo.com>
2018-06-12 11:19:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,11 +11,13 @@ function yourAnimal() {
   return animal
 }
 
+  const two = 2
+
 function add2(n) {
   return n + two
 
   // Feel free to move things around!
-  const two = 2
+
 }
 
 var funkyFunction = function() {
